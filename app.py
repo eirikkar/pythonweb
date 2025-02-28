31fbd9936ebb2298948e37ac1fc7aa2d68f23248
eirikkar <eirikkar@proton.me>
2025-02-28 11:03:05 +0100

    add get method

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+
+items = [
+    {"id": 1, "name": "Item 1"},
+    {"id": 2, "name": "Item 2"},
+    {"id": 3, "name": "Item 3"},
+]
+
 @app.route("/", methods=["GET"])
 def home():
     return "API is running! Try /api/items"
+
+app.route("/api/items", methods=["GET"])
+def get_items():
+    return jsonify(items)
+
 if __name__ == "__main__":
     app.run(debug=True)
