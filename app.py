e193b9a8ca345febd584c60e0657a07fee4b27b7
eirikkar <eirikkar@proton.me>
2025-02-28 11:03:20 +0100

    add get by id method

--- app.py
+++ app.py
@@ -16,5 +16,11 @@ app.route("/api/items", methods=["GET"])
 def get_items():
     return jsonify(items)
 
+app.route("/api/items/<int:id>", methods=["GET"])
+def get_item(id):
+    item = next((item for item in items if item["id"] == id), None)
+    if item:
+        return jsonify(item)
+    return jsonify({"message": "Item not found"}), 404
 if __name__ == "__main__":
     app.run(debug=True)
